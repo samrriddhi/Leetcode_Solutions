a8809212fad5b876cf46d7c12be751fd5645484f
Samriddhi Chauhan <samriddhichauhan27@gmail.com>
2026-03-26 19:47:31 +0530

    Added LeetCode 283 Move Zeroes

---
+++ Two Pointers/MoveZeroes.java
@@ -0,0 +1,16 @@
+// LeetCode 283 - Move Zeroes
+// Approach: Two Pointers
+// Time: O(n), Space: O(1)
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+        }
+    }
+}
