45cf55f69be33ca2c459e0aff2439e45992d1baf
Samriddhi Chauhan <samriddhichauhan27@gmail.com>
2026-03-28 02:19:56 +0530

    Added First and Last position of an element in an sorted array

---
+++ Binary Search/FindFirstAndLastPositionOfAnElementInSortedArray.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int start =0;
+        int end = nums.length-1;
+        int first = -1;
+        int last = -1;
+        //for first occurence
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                first=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        start=0;
+        end=nums.length-1;
+        //for last occurence
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                last=mid;
+                start=mid+1;;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return new int[]{first,last};
+    }
+}
