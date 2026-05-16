b04df9a0b74346fb26e30a1bd19b7982b873dc44
Samriddhi Chauhan <samriddhichauhan27@gmail.com>
2026-05-17 00:46:52 +0530

    Added a new file

---
+++ Binary Search/Sqrt(x).java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        int ans=0;
+        int low=1;
+        int high=x;
+        if(x==0 || x == 1){
+            return x;
+        }
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long square =(long)mid*mid;
+            if(square==x){
+                return mid;
+            }            
+            else if(square>x){
+                
+                high=mid-1;
+            }
+            else{
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+   }    
+}
