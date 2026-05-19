e20b80ee5269d447e6489668f24002f473b36bac
Samriddhi Chauhan <samriddhichauhan27@gmail.com>
2026-05-19 12:56:12 +0530

    New approach

--- Two Pointers/TwoSumII.java
+++ Two Pointers/TwoSumII.java
@@ -15,3 +15,30 @@ class Solution {
         return new int[]{};
     }
 }
+
+
+///* Two Pointers Solution */
+/*class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while(left < right) {
+
+            int sum = numbers[left] + numbers[right];
+
+            if(sum == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return new int[]{};
+    }
+}*/
