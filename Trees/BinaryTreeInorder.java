80d23078bc37b37337c4886d03a66e858e0e8fdb
Samriddhi Chauhan <samriddhichauhan27@gmail.com>
2026-06-06 00:58:47 +0530

    Added a new file

---
+++ Trees/BinaryTreeInorder.java
@@ -0,0 +1,16 @@
+class Solution {
+    List<Integer> result =new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        inorder(root);
+        return result;
+    }
+    private void inorder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        result.add(root.val);
+        inorder(root.right);
+    }
+}
