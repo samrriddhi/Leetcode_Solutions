b42d6ad45f9e66924e09b2d82661b6dd58035600
Samriddhi Chauhan <samriddhichauhan27@gmail.com>
2026-06-27 08:33:32 +0530

    added a new file

---
+++ Sliding Window/MaxSumofDistinctSubarraysWithLengthK.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        long sum=0;
+        long max=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            sum += nums[i];
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
+            if(i>=k){
+                sum -=nums[i-k];
+                map.put(nums[i-k],map.get(nums[i-k])-1);
+                if(map.get(nums[i-k])==0){
+                    map.remove(nums[i-k]);
+                }
+            }
+            if(i>=k-1){
+                if(map.size()==k){
+                    max=Math.max(max,sum);
+                }
+            }
+        }
+        return max;
+    }
+}
