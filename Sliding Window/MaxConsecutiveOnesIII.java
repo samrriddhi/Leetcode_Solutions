54bc05d772f3f8f09e51dcbd3a67f14af5027883
Samriddhi Chauhan <samriddhichauhan27@gmail.com>
2026-06-29 00:28:50 +0530

    added a new file

---
+++ Sliding Window/MaxConsecutiveOnesIII.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left=0;
+        int max=0;
+        int zeroes=0;
+        for(int right = 0 ; right<nums.length;right++){
+            if(nums[right]==0){
+                zeroes++;
+            }
+            while(zeroes>k){
+                if(nums[left]==0){
+                    zeroes--;
+                }
+                left++;
+            }
+            max=Math.max(max,right-left+1);
+        }
+        return max;
+    }
+}
